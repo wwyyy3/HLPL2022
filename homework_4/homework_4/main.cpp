92f26a5822268c1ecf7e8581ca58382a62a90c22
wwyyy3 <77263425+wwyyy3@users.noreply.github.com>
2022-02-12 23:25:01 +0100

    try this

---
+++ homework_4/homework_4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  homework_4
+//
+//  Created by xuan on 2022/2/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
