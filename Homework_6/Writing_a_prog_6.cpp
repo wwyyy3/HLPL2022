5d0f1b9d95bd7d16938b72cae7fdc5b23835013d
wwyyy3 <77263425+wwyyy3@users.noreply.github.com>
2022-02-24 11:43:29 +0100

    ..

---
+++ Homework_6/Writing_a_prog_6.cpp
@@ -0,0 +1,8 @@
+//
+//  Writing_a_prog_6.cpp
+//  homework_4
+//
+//  Created by xuan on 2022/2/24.
+//
+
+#include "Writing_a_prog_6.hpp"
---
+++ Homework_6/Writing_a_prog_6.hpp
@@ -0,0 +1,13 @@
+//
+//  Writing_a_prog_6.hpp
+//  homework_4
+//
+//  Created by xuan on 2022/2/24.
+//
+
+#ifndef Writing_a_prog_6_hpp
+#define Writing_a_prog_6_hpp
+
+#include <stdio.h>
+
+#endif /* Writing_a_prog_6_hpp */
